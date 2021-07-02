0005b757a8cde69eafca943fa5990c81020a667d
Aman Raj <amanpie9@gmail.com>
2021-07-02 10:31:47 +0530

    some minor changes in tasks_screen.dart

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:to_do/models/task_data.dart';
 import 'package:to_do/widgets/tasks_list.dart';
 import 'add_task_screen.dart';
-import 'package:to_do/models/tasks.dart';
 import 'package:provider/provider.dart';
 
 // ignore: camel_case_types

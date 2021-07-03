8dd6795869b6f5215294ee43dfa0b1684a6f7f4e
Aman Raj <amanpie9@gmail.com>
2021-07-03 13:01:53 +0530

    app bar defined

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -10,6 +10,73 @@ class Tasks_Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 80,
+        // title: Text(
+        //   "aman",
+        //   style: TextStyle(color: Colors.black),
+        // ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      extendBodyBehindAppBar: true,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+              ),
+              currentAccountPictureSize: Size.fromRadius(35),
+              accountName: Text(
+                "To Do",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+              accountEmail: Text("Your own personal ToDo"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.check_box_outline_blank_rounded),
+              title: Text('Check All'),
+              enabled: true,
+              onTap: () {
+                print("hello");
+              },
+            ),
+            ListTile(
+              onTap: null,
+              leading: Icon(Icons.check_box_outline_blank),
+              title: Text('UnCheck All'),
+            ),
+            ListTile(
+              onTap: null,
+              leading: Icon(Icons.delete),
+              title: Text('Delete All'),
+            ),
+            ListTile(
+              onTap: null,
+              leading: Icon(Icons.color_lens_sharp),
+              title: Text('Change Theme'),
+            ),
+            ListTile(
+              onTap: null,
+              leading: Icon(Icons.info),
+              title: Text('About'),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.deepOrange,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -27,22 +94,25 @@ class Tasks_Screen extends StatelessWidget {
           children: [
             Container(
               padding: EdgeInsets.only(
-                  top: 50.0, left: 30, right: 30.0, bottom: 30.0),
+                  top: 10.0, left: 30, right: 30.0, bottom: 30.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    foregroundColor: Colors.deepOrange,
-                    radius: 30.0,
-                    child: Icon(
-                      Icons.list,
-                      size: 30.0,
-                    ),
-                    backgroundColor: Colors.white,
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
+                  // GestureDetector(
+                  //   onTap: () {
+                  //     Scaffold.of(context).openDrawer();
+                  //   },
+                  //   child: CircleAvatar(
+                  //     foregroundColor: Colors.deepOrange,
+                  //     radius: 30.0,
+                  //     child: Icon(
+                  //       Icons.list,
+                  //       size: 30.0,
+                  //     ),
+                  //     backgroundColor: Colors.white,
+                  //   ),
+                  // ),
+
                   Text(
                     "To Do",
                     style: TextStyle(
